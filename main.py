8c73f89abf9d826790a5024de684c773db2ffe09
Mark Rogers <mark.rogers@givecampus.com>
2025-03-19 17:40:43 -0500

    project stats

--- main.py
+++ main.py
@@ -28,15 +28,26 @@ opts = {}
 
 # PLATFORM_SPRINT_ID = '1205117034782288' # NOPE
 PLATFORM_SPRINT_ID = "1203953156205290"
+BACKLOG_PROJ_ID = "1205425343659456"
 ESTIMATED_POINTS_GID = "1202399449289965"
 POINTS_GID = "1199932036644229"
 SUPPORT_BOARD_ID = "1201157086826331"
+
+TAG_DICT = {
+    "1209562201506706": "pod-og",
+    "1209562201506707": "pod-as",
+    "1209562201506708": "pod-platform",
+    "1209562201506710": "pod-unknown",
+    "1209562201506709": "pod-events",
+}
 
 
 @click.command()
 @click.option("--support-board", is_flag=True, help="List tasks for the support board")
 @click.option("--code-orange", is_flag=True, help="List tasks for Code Orange")
 @click.option("--velo-tracking", is_flag=True, help="List tasks for velocity tracking")
+@click.option("--support-tags", is_flag=True, help="List tasks for support tags")
+@click.option("--project-stats", is_flag=True, help="List all tasks for a specific project")
 @click.option(
     "-p", "--project-id", default=None, help="Filter tasks for this project id"
 )
@@ -52,6 +63,8 @@ def asana_router(
     support_board,
     code_orange,
     velo_tracking,
+    support_tags,
+    project_stats,
     project_id,
     completed_since,
     specific_tasks,
@@ -63,8 +76,12 @@ def asana_router(
         code_orange_stats()
     elif velo_tracking:
         velocity_tracking(completed_since, project_id)
+    elif support_tags:
+        support_board_tags()
     elif specific_tasks:
         get_specific_tasks(task_id_file)
+    elif project_stats:
+        project_tracking(project_id)
     else:
         print("No command specified")
 
@@ -137,11 +154,59 @@ def support_board_stats():
         },
     )
     print("name,completed,completed_at,completed_by,permalink_url,assignee.name")
+    import ipdb; ipdb.set_trace()
     for x in tasks:
         assignee = x["assignee"] or {}
         print(
             f"{x['name']},{x['completed']},{x['completed_at']},{x['completed_by']},{x['permalink_url']},{assignee.get("name", "unknown")}"
         )
+
+def support_board_tags():
+    TAGS = ["1209562201506706", "1209562201506707", "1209562201506708", "1209562201506710", "1209562201506709"]
+    tasks_api = asana.TasksApi(api_client)
+    for id, name in TAG_DICT.items():
+        tasks = tasks_api.get_tasks_for_tag(id, {
+            "opt_fields": "name,completed,completed_at,completed_by,permalink_url,assignee.name,custom_fields",
+        })
+        print(f"Tag: {name}, id: {id}, tasks: {len(list(tasks))}")
+
+@click.option(
+    "--project-id", default=BACKLOG_PROJ_ID, help="Filter tasks for this project id"
+)
+def project_tracking(project_id=None):
+    if not project_id:
+        project_id = BACKLOG_PROJ_ID
+
+    tasks_api = asana.TasksApi(api_client)
+    tasks = tasks_api.get_tasks_for_project(
+        project_id,
+        {
+            "opt_fields": "name,completed,completed_at,completed_by,permalink_url,custom_fields",
+        },
+    )
+    epic_dict = defaultdict(dict)
+    count = 0
+    for task in tasks:
+        count += 1
+        epic_field = [y for y in task['custom_fields'] if y['name'] == 'Eng Epic'][0]
+        try:
+            epic_name = epic_field['enum_value']['name']
+        except TypeError:
+            epic_name = "Not Set"
+        if task.get('completed'):
+            dict_key = 'completed'
+        else:
+            dict_key = 'incomplete'
+
+        current_count = epic_dict[epic_name].get(dict_key, 0)
+        try:
+            epic_dict[epic_name][dict_key] = current_count + 1
+        except KeyError:
+            epic_dict[epic_name][dict_key] = 1
+
+    print(f"Total tasks: {count}")
+    import ipdb; ipdb.set_trace()
+    print('hi')
 
 
 @click.option(
@@ -162,7 +227,7 @@ def velocity_tracking(completed_since=None, project_id=None):
         project_id = PLATFORM_SPRINT_ID
 
     tasks = tasks_api.get_tasks_for_project(
-        PLATFORM_SPRINT_ID,
+        project_id,
         {
             "opt_fields": "name,status,completed,completed_by,completed_at,custom_fields,permalink_url",
             "completed": True,
